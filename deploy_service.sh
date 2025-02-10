04696db9b34a146fe845157ddb5b1b2ee323b3ca
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2025-02-10 15:11:05 +0530

    fargate deployment changes

---
+++ deploy_service.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eo pipefail
+APP_IMAGE_NAME=$APPNAME
+ECS_REPONAME=$AWS_REPOSITORY
+ECS_TAG="latest"
+eval $(aws ecr get-login --region $AWS_REGION --no-include-email)
+docker tag $APP_IMAGE_NAME:$ECS_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_BUILD_NUM
+ECS_TAG=$CIRCLE_BUILD_NUM
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECS_REPONAME:$ECS_TAG
+ecs-cli configure --region us-east-1 --cluster $AWS_ECS_CLUSTER
+ecs-cli compose --project-name "$AWS_ECS_SERVICE_NAME" service up --launch-type FARGATE
