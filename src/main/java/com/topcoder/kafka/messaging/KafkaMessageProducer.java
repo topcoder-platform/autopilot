d61e50a91764d55b278aaf3b1efec59996bbeb5c
Justin Gasper <jmgasper@gmail.com>
2025-04-08 13:38:03 +1000

    Java version update

--- src/main/java/com/topcoder/kafka/messaging/KafkaMessageProducer.java
+++ src/main/java/com/topcoder/kafka/messaging/KafkaMessageProducer.java
@@ -5,12 +5,12 @@ import com.topcoder.onlinereview.component.jwt.JWTTokenGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.PrintWriter;
